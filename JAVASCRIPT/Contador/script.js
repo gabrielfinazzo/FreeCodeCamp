59d3275c3cc2ef634dac46b5c34e431c3c85df65
Gabriel <107438394+gabrielfinazzo@users.noreply.github.com>
2024-11-25 02:25:47 -0300

    Atualização estudos JavaScript

--- JAVASCRIPT/Contador/script.js
+++ JAVASCRIPT/Contador/script.js
@@ -1,15 +1,20 @@
-let countEl = document.getElementById("count-el")
-let count = 0
+let countEl = document.getElementById("count-el");
+let count = 0;
+let saveEl = document.getElementById("save-el");
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1;
+    countEl.textContent = count;
 }
 
-// 1. Create a function, save(), which logs out the count when it's called
 function save() {
-    console.log(count);
+    let contStr = count + " - ";
+    saveEl.textContent += contStr;
+    countEl.textContent = 0;
+    count = 0;
 };
+
+
 
 
 /* let username = "per";
--- JAVASCRIPT/novosTestes/JS/script.js
+++ JAVASCRIPT/novosTestes/JS/script.js
@@ -1,6 +1,70 @@
 
-let welcomeEl = document.getElementById("welcome-el");
+/* let welcomeEl = document.getElementById("welcome-el");
 let name = "Gabriel";
 let greeting = "Hi, my name is ";
 
-welcomeEl.innerText = greeting + name + "!";
+
+welcomeEl.innerText = greeting + name + "!"; */
+
+
+//Ex 01:
+// Create two variables, firstName and lastName
+//let firstName = "Gabriel";
+//let lastName = "Finazzo";
+// Concatenate the two variables into a third variable called fullName
+//let fullName = firstName + " " +lastName;
+// Log fullName to the console
+//console.log(fullName);
+
+//Ex 02:
+//let name = "Linda";
+//let greeting = "Hi there"
+// Create a function that logs out "Hi there, Linda!" When called
+//function greetingName() {
+//    let nameGreeting = greeting + ", " + name + "!";
+//    console.log(nameGreeting);
+//}
+//greetingName();
+
+//Ex 03:
+//let myPoints = 3;
+
+//Create two functions, add3Points() and remove1Point(), and have them
+//add/remove points to/ from the myPoints variable
+
+//function add3Points() {
+//    myPoints += 3;
+//};
+
+//function remove1Point() {
+//    myPoints -= 1;
+//};
+
+//add3Points();
+//remove1Point();
+//add3Points();
+//remove1Point();
+//add3Points();
+// Call the functions to that the line below logs out 10
+//console.log(myPoints);
+
+
+//Ex 04:
+// Try to predict what each of the lines will log out;
+//console.log("2" + 2); // "22"
+//console.log(11 + 7); // 18
+//console.log(6 + "5"); // "65"
+//console.log("My points: " + 5 + 9); // My points: 14 - wrong / Result: "59"
+//console.log(2 + 2); // 4
+//console.log("11" + "14"); // "1114"
+
+//Ex 05:
+
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error"
+let errorMessage = document.getElementById("error");
+
+function purchase() {
+    errorMessage.textContent = "Something went wrong, please try again";
+};
