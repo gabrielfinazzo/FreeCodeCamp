bbb74883e6f52009918d5fc939f1ec4d5aed93dd
Gabriel <107438394+gabrielfinazzo@users.noreply.github.com>
2024-11-26 20:55:35 -0300

    Atualização blackjack

--- JAVASCRIPT/Exercicios02/blackjack/JS/script.js
+++ JAVASCRIPT/Exercicios02/blackjack/JS/script.js
@@ -4,16 +4,24 @@
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
 let firstCard = 10//parseInt(Math.random() * 10 + 2);
-let secondCard = 12//parseInt(Math.random() * 10 + 2);
+let secondCard = 11//parseInt(Math.random() * 10 + 2);
 let sum = firstCard + secondCard;
-console.log(firstCard);
-console.log(secondCard);
-console.log(sum);
+let hasBlackJack = false;
+let isAlive = true;
+let message;
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂");
+    message = "Do you want to draw a new card? 🙂";
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳");
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
 } else {
-    console.log("You're out of the game! 😭");
+    message = "You're out of the game! 😭";
+    isAlive = false;
 };
+
+console.log(message);
+
+// CASH OUT!
+//console.log(hasBlackJack);
+//console.log(isAlive)
