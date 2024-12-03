ac3783575ea013653e25e3fa0aec3189e7cddec7
Gabriel <107438394+gabrielfinazzo@users.noreply.github.com>
2024-12-03 17:24:59 -0300

    Atualização projeto chrome extension

---
+++ JAVASCRIPT/Exercicios02/templateString/JS/script.js
@@ -0,0 +1,9 @@
+const recipient = "James";
+const sender = "Gabriel";
+
+//const email = "Hey" + recipient + "! How is it going? Cheers Per"
+const email = `
+Hey ${recipient}!
+How is it going?
+Cheers ${sender}`
+console.log(email);
